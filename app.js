ec6e4219e2fb121640eec26c8e8c4c72fe36d231
erindoman <erin.doman@gmail.com>
2020-11-04 20:03:47 -0600

    solve exercise 12

--- app.js
+++ app.js
@@ -139,7 +139,15 @@ Exercise 12:
 
 // Complete Exercise 12 below...
 
+const fizz = [];
+const buzz = [];
+const fizzbuzz = []; 
 
+nums.forEach(function(num) {
+  if (num % 3 === 0) fizz.push(num);
+  if (num % 5 === 0) buzz.push(num);
+  if (num % 3 === 0 && num % 5 === 0) fizzbuzz.push(num);
+});
 
 console.log('Exercise 12 Results:');
 console.log('  fizz:', fizz);
